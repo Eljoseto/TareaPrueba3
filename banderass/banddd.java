2159c6e1a0d08725c1b6f36db63aa823cafab2fc
Vejat <84534302+Vejat@users.noreply.github.com>
2021-07-05 12:06:39 -0400

    Add files via upload

---
+++ banderass/banddd.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banderass;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author vejat
+ */
+public class banddd extends JFrame {
+    boolean bandera= true;
+    public banddd(){
+        setSize(500,500);
+        
+    }
+    public void ponerbandera(){
+        int a;
+    }
+    
+}
